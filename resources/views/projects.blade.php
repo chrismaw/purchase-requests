0067e314838d91f8ae73fb62f67122f88e5308f0
cmaw <christophermaw@outlook.com>
2019-07-04 15:04:44 -0400

    fixed special char bug

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -226,7 +226,7 @@
                             @foreach ($users as $user)
                                 @if ($user->id == Auth::user()->id)
                                 @else
-                                    { label: "addslashes({{ $user->name }})", value: "{{ $user->id }}" },
+                                    { label: "{{ addslashes($user->name) }}", value: "{{ $user->id }}" },
                                 @endif
                             @endforeach
                         ]
